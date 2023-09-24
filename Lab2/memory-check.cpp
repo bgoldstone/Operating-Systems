fe3c27e0128f93f0a613172a8d29a2d4307b3897
Benjamin Goldstone <bgoldstone1@gmail.com>
2023-09-24 14:57:54 -0400

    updated memory-check.cpp

--- Lab2/memory-check.cpp
+++ Lab2/memory-check.cpp
@@ -1,59 +1,69 @@
 #include <iostream>
 #include <random>
 #include <time.h>
+#include <map>
 
-void initArray(int *memory, int size);
-void printArray(int *memory, int size);
-void setMemory(int *memory, int size);
+/// @brief Array structure that contains array and size
+struct arrayStruct
+{
+    int *array;
+    int size;
+};
+
+void initArray(arrayStruct *memory);
+void printArray(arrayStruct *memory);
+void setMemory(arrayStruct *memory);
+void printMemory(arrayStruct *memory);
 
 int main()
 {
     // Memory Array.
     const int size = 20;
+    arrayStruct *array = new arrayStruct;
     int *memoryBytes = new int[size];
+    array->array = memoryBytes;
+    array->size = size;
     // Pointer to first memory element.
 
-    initArray(memoryBytes, size);
-    printArray(memoryBytes, size);
-    setMemory(memoryBytes, size);
-    printArray(memoryBytes, size);
+    initArray(array);
+    printArray(array);
+    setMemory(array);
+    printMemory(array);
 
     delete[] memoryBytes;
 
     return 0;
 }
 
-/// @brief initializes arrays to all zeros.
-/// @param memory array to modify.
-/// @param size size of array.
-void initArray(int *memory, int size)
+/// @brief initializes arrayStruct to all zeros.
+/// @param memory arrayStruct to modify.
+void initArray(arrayStruct *memory)
 {
-    for (int i = 0; i < size; i++)
+
+    for (int i = 0; i < memory->size; i++)
     {
-        memory[i] = 0;
+        memory->array[i] = 0;
     }
 }
 
 /// @brief Prints array in a readable format.
-/// @param memory array to print out.
-/// @param size size of the array.
-void printArray(int *memory, int size)
+/// @param memory arrayStruct to print out.
+void printArray(arrayStruct *memory)
 {
     std::cout << "[";
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < memory->size; i++)
     {
-        std::cout << memory[i] << ", ";
+        std::cout << memory->array[i] << ", ";
     }
     std::cout << "\b\b]" << std::endl;
 }
 
 /// @brief allocates "memory for processes"
-/// @param memory memory allocation to set.
-/// @param size size of memory.
-void setMemory(int *memory, int size)
+/// @param memory arrayStruct to allocate.
+void setMemory(arrayStruct *memory)
 {
-    int *currentByte = memory;
-    int *lastByte = &memory[size - 1] + 1;
+    int *currentByte = memory->array;
+    int *lastByte = &memory->array[memory->size - 1] + 1;
     // Seed random to current time.
     srand(time(NULL));
     int program = 1;
@@ -76,5 +86,30 @@ void setMemory(int *memory, int size)
             }
         }
         program++;
+    }
+}
+
+void printMemory(arrayStruct *memory)
+{
+    std::map<int, int> memoryAllocation;
+
+    // Find how many bytes each array took up.
+    for (int i = 0; i < memory->size; i++)
+    {
+        int programNum = memory->array[i];
+        if (memoryAllocation.find(programNum) != memoryAllocation.end())
+        {
+            memoryAllocation[programNum] = memoryAllocation[programNum] + 1;
+        }
+        else
+        {
+            memoryAllocation[programNum] = 1;
+        }
+    }
+
+    for (auto it = memoryAllocation.begin(); it != memoryAllocation.end(); it++)
+    {
+        printf("Program %d required %d bytes of memory.\n", it->first, it->second);
+        printArray(memory);
     }
 }
