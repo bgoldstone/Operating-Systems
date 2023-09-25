423b712e6957ac20d8209b5318519608f8ae403f
Benjamin Goldstone <bgoldstone1@gmail.com>
2023-09-25 16:31:39 -0400

    updated comments and formatting in tiny shell

--- Lab2/tiny-shell.cpp
+++ Lab2/tiny-shell.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::string command = DEFAULT_VALUE;
     int numOfFiles = 0;
-    std::cout << "Welcome to the tiny terminal! the worlds best terminal!" << std::endl;
+    std::cout << "Welcome to the tiny shell! the worlds best shell!" << std::endl;
 
     while (command != "exit")
     {
@@ -17,10 +17,11 @@ int main()
         }
         else if (command == "removeFile")
         {
-            if (numOfFiles != 0)
+            if (numOfFiles > 0)
             {
                 numOfFiles--;
             }
+            // Tells user that they cannot remove files since there are zero left.
             else
             {
                 std::cout << "Cannot remove file that does not exist!" << std::endl;
@@ -28,11 +29,24 @@ int main()
         }
         else if (command == "countFile")
         {
+            // Tells user the total number of files.
             std::cout << "Number of Files: " << numOfFiles << std::endl;
+        }
+        else if (command == "help")
+        {
+            std::cout << "Here are all of the available commands:\n";
+            std::cout << "<-------------------------FILE IO------------------------>\n";
+            std::cout << "\"addFile\" adds one file to the system\n";
+            std::cout << "\"removeFile\" removes one file from the system\n";
+            std::cout << "\"countFile\" Counts the number of files in the system.\n";
+            std::cout << "<-------------------------SYSTEM------------------------->\n";
+            std::cout << "\"help\" displays this helpful menu!\n";
+            std::cout << "\"exit\" exit tiny shell\n";
         }
         else if (command != DEFAULT_VALUE)
         {
-            std::cout << "Invalid Command!" << std::endl;
+            std::cout << "Command: \"" << command << "\" is an invalid Command!";
+            std::cout << "\n\nType \"help\" for a list of commands" << std::endl;
         }
         std::cout << ">";
         getline(std::cin, command);
